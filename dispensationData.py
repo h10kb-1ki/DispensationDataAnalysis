452e0375707445e8291a28416d49e5beee28793b
HM <hmillionsalt@gmail.com>
2024-07-30 17:04:06 +0900

    new chart

--- dispensationData.py
+++ dispensationData.py
@@ -4,76 +4,70 @@ import altair as alt
 
 st.title('外来処方の検証')
 with st.sidebar:
-    Pop = st.number_input('一包化加点', 5)
-    Pc = st.number_input('つぶし加点（1Rpごと）', 3)
     st.write('')
-    st.write('---要因---')
-    st.write('・軟膏の混合：未設定')
-    st.write('・薬品管理室に在庫がある薬：未設定')
-    st.write('・麻薬：未設定')
-    st.write('・毒薬（Revは追加加点）：未設定')
-    st.write('・特定生物由来：未設定')
+    st.write('---条件---')
+    st.write('1. 基本的には、1剤につき1点。棚配置に応じて以下のように配点。')
+    st.markdown('&nbsp;&nbsp;&nbsp;✓&nbsp;薬品管理室の薬剤は5点')  #'&nbsp;'はHTMLのスペース(st.write()では無視されるのでst.markdown())
+    st.markdown('&nbsp;&nbsp;&nbsp;✓&nbsp;麻薬、覚原、特生は4点')
+    st.markdown('&nbsp;&nbsp;&nbsp;✓&nbsp;毒薬、栄養剤は3点')
+    st.markdown('&nbsp;&nbsp;&nbsp;✓&nbsp;Revへの追加加点：未設定')
+    st.write('2. 一包化指示があれば＋7点')
+    st.write('3. つぶし指示があれば1剤につき＋5点')
+    st.write('4. 混合指示があれば1剤につき＋5点')
+    st.write('')
     st.write('---memo---')
-    st.write('・祝日フラグ')
-    st.write('・欠損値の処理（x軸が揃うように）')
-
-    df = pd.read_excel('調剤負荷データ.xlsx')
-    df['date'] = pd.to_datetime(df['更新日'])
-    df['調剤負荷'] = ''
-    for i in range(len(df)):
-        point_rp = df.loc[i, 'Rp数']
-        if df.loc[i, '一包化'] == 1:
-            point_op = Pop
-        else:
-            point_op = 0
-        point_crush = df.loc[i, 'つぶしRp数'] * Pc
-        df.loc[i, '調剤負荷'] = point_rp + point_op + point_crush
-    df = df[['更新日時', 'date', '更新時刻', '曜日', '調剤負荷']]
-
-st.dataframe(df)
-#---------------------------------------------------------
-st.subheader('指定日・時間ごとの調剤負荷', divider="gray")
-dates = sorted(list(set(df['更新日時'].tolist())))
-day = st.date_input('日を指定', dates[0], min_value=dates[0], max_value=dates[len(dates)-1])
-btn2 = st.button('時間ごとの調剤負荷')
-if btn2:
-    df2 = df[['更新日時', 'date', '更新時刻', '曜日', '調剤負荷']]
-    df2q = df2.query(f'date == "{day}"')
+    st.write('- 配点の検討')
+    st.write('- 祝日フラグ検討（6月は関係ない）')
+    st.write('- ヘルプ人数と照合')
+    st.write('- 待ち時間との整合性（調剤負荷グラフよりやや右にずれる）')
     
-    df2qp = pd.pivot_table(df2q, index='更新時刻', values='調剤負荷', aggfunc='sum')
-    df2qp.reset_index(inplace=True)
-    bars = alt.Chart(df2qp).mark_bar(size=25).encode(
-                x=alt.X('更新時刻', title='時間', axis=alt.Axis(labelAngle=-30)),
-                y=alt.Y('調剤負荷', title='調剤負荷'),
-                ).properties(
-                    width=450,
-                    height=350,
-                    title=f'時間ごとの調剤負荷  {day}'
-                    )
-    st.altair_chart(bars, use_container_width=True)
-    st.dataframe(df2qp)
-#---------------------------------------------------------
-st.write('')
-st.subheader('曜日・時間ごとの調剤負荷', divider="gray")
-weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-wd = st.selectbox('曜日を選択', weekdays)
-y_max = st.number_input('y軸最大値', 400)
-btn3 = st.button('曜日別の調剤負荷')
-if btn3:
-    df3 = df[['更新日時', 'date', '更新時刻', '曜日', '調剤負荷']]
-    df3q = df3.query(f'曜日 == "{wd}"')
-    df3qp = pd.pivot_table(df3q, index='date', columns='更新時刻', values='調剤負荷', aggfunc='sum')
-    df3qp = df3qp.T
-    df3qp.reset_index(inplace=True)
-    df3qp = df3qp.fillna(0)
-    df3qpm = pd.melt(df3qp, id_vars=['更新時刻'])
-    line = alt.Chart(df3qpm).mark_line().encode(
-                x='更新時刻',
-                y='mean(value)'
+df = pd.read_excel('調剤負荷データ.xlsx')
+
+col1, col2 = st.columns([0.3, 0.7], vertical_alignment='bottom')
+with col1:
+    auxiliary_line = st.number_input('補助線', value=275)
+with col2:
+    btn = st.button('表示')
+st.write('---')
+if btn:
+    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+    for wd in weekdays:
+        dfq = df.query(f'曜日 == "{wd}"')
+        dfqp = pd.pivot_table(dfq, index='更新日', columns='更新時刻', values='調剤負荷', aggfunc='sum')
+        dfqp = dfqp.T
+        dfqp.reset_index(inplace=True)
+        dfqp = dfqp.fillna(0)
+        dfqpm = pd.melt(dfqp, id_vars=['更新時刻'])
+        line = alt.Chart(dfqpm).mark_line().encode(
+            x='更新時刻',
+            y='mean(value)'
+        )
+        #折れ線グラフ
+        band = alt.Chart(dfqpm).mark_errorband(extent='ci').encode(
+            x=alt.X('更新時刻', title='', axis=alt.Axis(labelAngle=-30, grid=True)), 
+            y=alt.Y('mean(value)', title='', 
+                    scale=alt.Scale(domain=[0, 450])), 
+            ).properties(title=f'曜日ごとの調剤負荷（{wd}）', 
             )
-    band = alt.Chart(df3qpm).mark_errorband(extent='ci').encode(
-                x=alt.X('更新時刻', title='時間'),
-                y=alt.Y('mean(value)', title='調剤負荷', 
-                        scale=alt.Scale(domain=[0, y_max]))
+        rule = alt.Chart().mark_rule(strokeDash=[5, 5], size=1, color='red').encode(y=alt.datum(auxiliary_line))
+
+        st.altair_chart(band + line + rule, use_container_width=True)
+        #ヒートマップ
+        hm = alt.Chart(dfqpm).mark_rect().encode(
+            x=alt.X('更新時刻', title='', axis=alt.Axis(labelAngle=-30)), 
+            y=alt.Y('更新日', title=''), 
+            color=alt.Color('value:Q', scale=alt.Scale(domain=[0, 450], range=['azure', 'royalblue']))  # カラースケールを指定
+            ).properties(
+                width=550,
+                height=150
             )
-    st.altair_chart(band + line)
+        text = hm.mark_text(baseline='middle').encode(
+            text=alt.Text('value:Q', format='.0f'),
+            color=alt.condition(
+                alt.datum.value >= auxiliary_line,  # 値が基準より大きい場合は赤、そうでない場合は黒
+                alt.value('red'),
+                alt.value('black')
+            )
+        )
+        st.altair_chart(hm + text, use_container_width=True)
+        st.write('---')
